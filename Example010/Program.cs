77be10b24d79dba5833e4ba78ef496e25850c0ff
Kiesaro <freestylerspb@gmail.com>
2022-12-25 02:56:24 +0300

    Example010

---
+++ Example010/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+char[] array = number.ToString().ToCharArray();
+Console.WriteLine(array[1]);
+
+
+
