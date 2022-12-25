94f4b1af297a8adb40189dfce7b212aa0df2f230
Kiesaro <freestylerspb@gmail.com>
2022-12-25 03:24:42 +0300

    Example014

---
+++ Example014/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите день недели");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<8)
+{
+    if (number == 6 || number == 7)
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    {
+        Console.WriteLine("Будний");
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка");
+}
