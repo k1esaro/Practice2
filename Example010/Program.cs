45065e053a8f3a075f90d24e2e4934d2ffba85d3
Kiesaro <freestylerspb@gmail.com>
2022-12-25 03:06:41 +0300

    Example010.1

--- Example010/Program.cs
+++ Example010/Program.cs
@@ -1,7 +1,8 @@
 ﻿Console.WriteLine("Введите трехзначное число");
 int number = Convert.ToInt32(Console.ReadLine());
-char[] array = number.ToString().ToCharArray();
-Console.WriteLine(array[1]);
+int n1 = number/100 * 10;
+int result = number/10 - n1;
+Console.WriteLine(result);
 
 
 
